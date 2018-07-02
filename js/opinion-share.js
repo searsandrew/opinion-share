e267ab9f6cbaabb25a6d9422f95794f4133cd9c8
Andrew Sears <andrew@mayfifteenth.com>
2018-07-02 00:27:12 -0500

    Rebuilt slideouts to use vanilla javascript and simplified

--- js/opinion-share.js
+++ js/opinion-share.js
@@ -1,12 +1,23 @@
-(function($){
-	$(document).ready(function(){
-	  $.slidebars();
-	});
-}) (jQuery);
+function openLeftNav() {
+	document.getElementById("osLeftBar").style.width = "250px";
+	document.getElementById("page").style.marginLeft = "250px";
+	// document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
 
-jQuery('.img-responsive').click(function(){
-	jQuery('.page-opinionshare').animate({
-		left: "+=250"
-	}, 5000, function(){	
-	});
-})
+function openRightNav() {
+	document.getElementById("osRightBar").style.width = "250px";
+	document.getElementById("page").style.marginRight = "250px";
+	// document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeLeftNav() {
+	document.getElementById("osLeftBar").style.width = "0";
+	document.getElementById("page").style.marginLeft= "0";
+	// document.body.style.backgroundColor = "white";
+}
+
+function closeRightNav() {
+	document.getElementById("osRightBar").style.width = "0";
+	document.getElementById("page").style.marginRight= "0";
+	// document.body.style.backgroundColor = "white";
+}
--- opinion-share.php
+++ opinion-share.php
@@ -11,11 +11,37 @@ require_once($dir.'inc/shortcode.php');
 require_once($dir.'inc/admin-opinionshare.php');
 require_once($dir.'inc/templater.php');
 
+add_action('wp_head', 'cfp_do_action_sidebars', 10, 1);
+add_action('setup_bars', 'cfp_setup_sidebars', 15, 1);
+
+function cfp_setup_sidebars($options){
+	print_r($options); ?>
+	<div id="osRightBar" class="sidenav sidenavRight">
+		<a href="javascript:void(0)" class="closebtn" onclick="closeRightNav()">&times;</a>
+		<?php if($options['cfp_opinionshare_gformchk_0'] == 1 && isset($options['cfp_opinionshare_gform_0'])){
+			$gformid = $options['cfp_opinionshare_gform_0'];
+			gravity_form($gformid, true, true, false, '', true);
+		} ?>
+	</div>
+
+	<div id="osLeftBar" class="sidenav sidenavLeft">
+		<a href="javascript:void(0)" class="closebtn" onclick="closeLeftNav()">&times;</a>
+		<h3 class="text-center text-slidebar-title" style="line-height: 34px;"><?= strtoupper($options['cfp_opinionshare_ltitle']); ?></h3>
+		<?php echo $options['opinionshare_ldesc'] != ''?'<p class="text-slidebar-desc">'.$options['opinionshare_ldesc'].'</p>':''; ?>
+	</div>
+<?php
+};
+
+function cfp_do_action_sidebars() {
+	$options = get_option('cfp_opinionshare_options');
+	do_action('setup_bars', $options);
+}
+
 function cfp_opinion_scripts(){
 	wp_enqueue_script( 'opinionsharejs', '/wp-content/plugins/opinion-share/js/opinion-share.js', array('jquery'), '1.0.0', true );
-	wp_enqueue_script( 'slidebarsjs', '/wp-content/plugins/opinion-share/js/slidebars.min.js', array('jquery'), '0.10.3', true );
+//	wp_enqueue_script( 'slidebarsjs', '/wp-content/plugins/opinion-share/js/slidebars.min.js', array('jquery'), '0.10.3', true );
 	wp_enqueue_style( 'opinionsharecss', '/wp-content/plugins/opinion-share/css/opinionshare.css', false, '1.0.0', 'all');
-	wp_enqueue_style( 'slidebarscss', '/wp-content/plugins/opinion-share/css/slidebars.min.css', false, '0.10.3', 'all');
+//	wp_enqueue_style( 'slidebarscss', '/wp-content/plugins/opinion-share/css/slidebars.min.css', false, '0.10.3', 'all');
 }
 add_action( 'wp_enqueue_scripts', 'cfp_opinion_scripts' );
 
