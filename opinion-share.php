49fa58e2fdef0ea9f4094a93e7a75084cd80d1c8
Andrew Sears <andrew@mayfifteenth.com>
2018-07-01 19:53:38 -0500

    Update Page Templater to be WordPress 4.7+ compatible

--- opinion-share.php
+++ opinion-share.php
@@ -1,6 +1,6 @@
 <?php /*
 Plugin Name: Opinion Share
-Plugin URI:  http://www.centreforge.us/opinion-share
+Plugin URI:  https://github.com/searsandrew/opinion-share
 Description: Adds a page template and shortcode to Wordpress that asks for visitor opinion, then sends them to either 'Share' a review, or 'Give Feedback'.
 Version:     1.0.20
 Author:      Andrew Sears
@@ -9,64 +9,7 @@ Author URI:  http://www.mayfifteenth.com
 $dir = plugin_dir_path( __FILE__ );
 require_once($dir.'inc/shortcode.php');
 require_once($dir.'inc/admin-opinionshare.php');
-
-class PageTemplater {
-	protected $plugin_slug;
-	private static $instance;
-	protected $templates;
-	public static function get_instance(){
-		if(null == self::$instance){
-			self::$instance = new PageTemplater();
-		} 
-		return self::$instance;
-	} 
-	private function __construct(){
-		$this->templates = array();
-		add_filter(
-			'page_attributes_dropdown_pages_args',
-			 array( $this, 'register_project_templates' ) 
-		);
-		add_filter(
-			'wp_insert_post_data', 
-			array( $this, 'register_project_templates' ) 
-		);
-		add_filter(
-			'template_include', 
-			array( $this, 'view_project_template') 
-		);
-		$this->templates = array(
-			'page-opinionshare.php' => 'Opinion Share Page',
-		);
-	} 
-	public function register_project_templates($atts){
-		$cache_key = 'page_templates-' . md5( get_theme_root() . '/' . get_stylesheet() );
-		$templates = wp_get_theme()->get_page_templates();
-		if ( empty( $templates ) ) {
-			$templates = array();
-		} 
-		wp_cache_delete( $cache_key , 'themes');
-		$templates = array_merge( $templates, $this->templates );
-		wp_cache_add( $cache_key, $templates, 'themes', 1800 );
-		return $atts;
-	} 
-	public function view_project_template($template){
-		global $post;
-		if (!isset($this->templates[get_post_meta( 
-			$post->ID, '_wp_page_template', true 
-		)] ) ) {
-		return $template;
-	} 
-	$file = plugin_dir_path(__FILE__). get_post_meta( 
-		$post->ID, '_wp_page_template', true 
-	);
-	if( file_exists( $file ) ) {
-		return $file;
-	} 
-	else { echo $file; }
-		return $template;
-	} 
-} 
-add_action('plugins_loaded', array('PageTemplater', 'get_instance'));
+require_once($dir.'inc/page-templater.php');
 
 function cfp_opinion_scripts(){
 	wp_enqueue_script( 'opinionsharejs', '/wp-content/plugins/opinion-share/js/opinion-share.js', array('jquery'), '1.0.0', true );
