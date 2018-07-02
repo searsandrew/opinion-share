9ad3d57494d0dbe8b4acb48aa7b7bdaa388ea35c
Andrew Sears <andrew@mayfifteenth.com>
2018-07-02 00:35:32 -0500

    Completely remove slidebars

--- opinion-share.php
+++ opinion-share.php
@@ -39,9 +39,7 @@ function cfp_do_action_sidebars() {
 
 function cfp_opinion_scripts(){
 	wp_enqueue_script( 'opinionsharejs', '/wp-content/plugins/opinion-share/js/opinion-share.js', array('jquery'), '1.0.0', true );
-//	wp_enqueue_script( 'slidebarsjs', '/wp-content/plugins/opinion-share/js/slidebars.min.js', array('jquery'), '0.10.3', true );
 	wp_enqueue_style( 'opinionsharecss', '/wp-content/plugins/opinion-share/css/opinionshare.css', false, '1.0.0', 'all');
-//	wp_enqueue_style( 'slidebarscss', '/wp-content/plugins/opinion-share/css/slidebars.min.css', false, '0.10.3', 'all');
 }
 add_action( 'wp_enqueue_scripts', 'cfp_opinion_scripts' );
 
